d69c54281ee5c89e910dad19e16886c94a3a8949
Adam Steele <adamsteele@google.com>
2020-09-10 16:48:28 +0000

    vm_count_test_two

--- multi_run.py
+++ multi_run.py
@@ -3,26 +3,40 @@ import subprocess
 import sys
 from multiprocessing import Pool
 
-if __name__ == '__main__':
-    files = os.listdir(sys.argv[1])
+
+def find_metdata(file_path):
     metadata = ''
-    with open(os.path.join(sys.argv[1], 'base.yaml')) as f:
+    with open(file_path) as f:
         for line in f.readlines():
             if 'metadata' in line:
-                metadata = line.split('|')[1].strip()
-                print(metadata)
-                break
+                metadata += line.split('|')[1].strip()
+    return metadata
+
+
+if __name__ == '__main__':
+    folder = sys.argv[1]
+    files = [f for f in os.listdir(folder)]
+
+    # Extract metadata from base
+    base_metadata = ''
+    if 'base.yaml' in files:
+        print('Found base file. Extracting base_metadata')
+        base_metadata = find_metdata(os.path.join(folder, 'base.yaml'))
+        files.remove('base.yaml')
+
     with Pool(3) as p:
         parallel_tests = []
+
         for f in files:
-            if f == 'base.yaml':
-                continue
-            fp = os.path.join(sys.argv[1], f)
-            args = ['./pkb.py',
-                    '--benchmark_config_file={}'.format(fp),
-                    '--metadata={}'.format(metadata)]
-            if len(sys.argv) > 2:
-                for s in sys.argv[2:]:
-                    args.append(s)
+            fp = os.path.join(folder, f)
+
+            metadata = ','.join([base_metadata, find_metdata(fp), 'yaml:{}'.format(f)])
+
+            base_args = ['./pkb.py',
+                         '--benchmark_config_file={}'.format(fp),
+                         '--metadata={}'.format(metadata)]
+            args = base_args + sys.argv[2:]
+
             parallel_tests.append(args)
+
         p.map(subprocess.call, parallel_tests)
